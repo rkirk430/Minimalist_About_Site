59abc30786d74d43e62de3f6ebc114e9a42e25dc
rkirk430 <r.kirk430@gmail.com>
2022-06-16 15:27:44 -0400

    Homepage Icons

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,26 @@
 import React from "react";
-import Social from "../Social";
+import Social from "../social/Social";
 import Headshot from '../../assets/headshot_picture1.png'
 
 const Home = () => {
   return (
     <>
       <div className="homepage__home">
-        <img src={Headshot} alt="Headshot"/>
-        <h3> Hello </h3>
+        <div className="homepage__content">
+          {/* Start of Image */}
+          <div className="homepage__content-picture">
+            <img src={Headshot} alt="Headshot"/>
+          </div>
+          {/* End of Image */}
+
+          {/* Start of Details Section */}
+          <div className="homepage__content-details">
+            <h3 className="homepage__content-details-name"> RJ Kirk </h3>
+            <p className="homepage__content-details-job"> Creative photographer based out of Europe </p>
+          </div>
+          <Social />
+          {/* End of Details Section */}
+        </div>
       </div>
 
     </>
---
+++ src/components/social/Social.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {FiDribbble} from 'react-icons/fi'
+
+
+const Social = () => {
+  return (
+    <div className="header__socials">
+      <a href="https://www.linkedin.com/in/robert-kirk1/" target = "_blank" rel = "noreferrer" ><BsLinkedin /></a>
+      <a href = "https://github.com/rkirk430" target = "_blank" rel = "noreferrer"><FaGithub /></a>
+      <a href = "https://dribble.com" target = "_blank" rel = "noreferrer"><FiDribbble /></a>
+    </div>
+  )
+}
+
+export default Social
+
